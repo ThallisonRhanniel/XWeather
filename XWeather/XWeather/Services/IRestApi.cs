914c121f266ecb082d9a5a4aa9b410d85701cb38
Thallison Rhanniel Garcia Lima <thallisonrlima@gmail.com>
2020-03-09 22:16:53 -0300

    Adicionado rest api.

---
+++ XWeather/XWeather/Services/IRestApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+using XWeather.Models;
+
+namespace XWeather.Services
+{
+    public interface IRestApi
+    {
+        [Get("/183813cbaffd14171cf5df835f806262/{latitude},{longitude}?exclude=flags&lang=pt&units=auto")]
+        Task<WeatherModel> GetWeather(double latitude, double longitude);
+
+    }
+}
